0f9e0420d32240262898218eead6e974c9e1570d
dohaehab <dohaehab@gmail.com>
2025-01-24 23:24:14 +0200

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# This script sets up the web servers for deployment
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update && sudo apt-get install nginx -y
+fi
+
+if [ ! -d "/data" ]; then
+    mkdir -p /data
+fi
+
+if [ ! -d "/data/web_static/" ]; then
+    mkdir -p /data/web_static/
+fi
+
+if [ ! -d "/data/web_static/releases/" ]; then
+    mkdir -p /data/web_static/releases/
+fi
+
+if [ ! -d "/data/web_static/shared/" ]; then
+    mkdir -p /data/web_static/shared/
+fi
+
+if [ ! -d "/data/web_static/releases/test/" ]; then
+    mkdir -p /data/web_static/releases/test/
+fi
+
+echo "<!DOCTYPE html>
+<html>
+<body>
+    <h1>ALX</h1>
+</body>
+</html>
+" > /data/web_static/releases/test/index.html
+
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+fi
+ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+
+chown -R ubuntu:ubuntu "/data"
+
+printf "%s\n" "server {
+    listen 80;
+    server_name _;
+    
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+}" > /etc/nginx/sites-available/default
+systemctl restart nginx
+
