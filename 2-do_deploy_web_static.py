011d31fc175558f567602e71100807aa74eacd5e
dohaehab <dohaehab@gmail.com>
2025-01-25 02:24:38 +0200

    task 1

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This script generates tgz archive """
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = f"web_static_{timestamp}.tgz"
+        archive_path = f"versions/{archive_name}"
+
+        local(f"tar -cvzf {archive_path} web_static")
+
+        return archive_path
+    except Exception as e:
+        return None
