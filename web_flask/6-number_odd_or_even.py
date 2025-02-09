8b3ba169b478ef9ccebdff6f207366a789d4904e
dohaehab <dohaehab@gmail.com>
2025-02-09 23:20:04 +0200

    task 0

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ start a flask web page """
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -31,10 +31,16 @@ def print_python_text(text="is cool"):
     return f'Python {escape(text)}'.replace("_", " ")
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def print_number(n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def print_number_template(n):
     """ Return number """
-    return f'{escape(n)} is a number'
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def print_number_type(n):
+    """ Return number """
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
